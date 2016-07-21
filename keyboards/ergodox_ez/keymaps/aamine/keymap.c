3cbc9e25b433799eb6a3774435c802ea4fff2a8c
Minero Aoki <minero-aoki@cookpad.com>
2016-07-21 17:31:48 +0900

    fix keymap

--- keyboards/ergodox_ez/keymaps/aamine/keymap.c
+++ keyboards/ergodox_ez/keymaps/aamine/keymap.c
@@ -18,14 +18,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|      |           | PgDn |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | Enter |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | Ctrl |   [  |   ]  |  Alt |  Cmd |                                       |  <-  | Down |  Up  |  ->  | Enter|
+ *   | Ctrl |  <-  |  ->  |  Alt |  Cmd |                                       | Down |  Up  |   [  |   ]  | Enter|
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        |      | RESET|       | Home | End  |
  *                                 ,------|------|------|       |------+--------+------.
- *                                 |      |      |      |       |      |        |      |
- *                                 | Cmd  | Ctrl |------|       |------| Space  | Back |
- *                                 |      |      |Shift |       | Cmd  |        | Space|
+ *                                 |      |      |      |       |      | Back   |      |
+ *                                 | Cmd  | Ctrl |------|       |------| Space  | Space|
+ *                                 |      |      |Shift |       | Cmd  |        |      |
  *                                 `--------------------'       `----------------------'
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
@@ -34,9 +34,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // left hand
         KC_ESC,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_NO,
         KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   TG(SYMB),
-        KC_GRV,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
+        KC_GRV,         KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_NO,
-        KC_LCTL,        KC_LBRC,      KC_RBRC,KC_LALT,KC_LGUI,
+        KC_LCTL,        KC_LEFT,      KC_RGHT,KC_LALT,KC_LGUI,
                                                       KC_NO,   RESET,
                                                                KC_NO,
                                              KC_LGUI, KC_LCTL, KC_LSFT,
@@ -45,10 +45,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              KC_PGUP,     KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
                           KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,          KC_QUOT,
              KC_PGDN,     KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          KC_ENT,
-                                  KC_LEFT,KC_DOWN,KC_UP,  KC_RGHT,          KC_ENT,
+                                  KC_DOWN,KC_UP,  KC_LBRC, KC_RBRC,         KC_ENT,
              KC_HOME, KC_END,
              KC_NO,
-             KC_LGUI, KC_SPC, KC_BSPC
+             KC_LGUI, KC_BSPC, KC_SPC
     ),
 /* Keymap 1: Symbol Layer
  *
